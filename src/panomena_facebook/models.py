e88d9600653f2152805dc3fec9958fb0fb2f2e9c
Jurie-Jan Botha <juriejanbotha@gmail.com>
2011-07-04 14:09:33 +0200

    Added related name for profile to site

--- src/panomena_facebook/models.py
+++ src/panomena_facebook/models.py
@@ -7,7 +7,7 @@ from django.contrib.sites.models import Site
 class FacebookProfile(models.Model):
     """Facebook profile detail for a user."""
     user = models.OneToOneField(User, related_name='facebook_profile')
-    site = models.ForeignKey(Site, default=Site.objects.get_current)
+    site = models.ForeignKey(Site, default=Site.objects.get_current, related_name='facebook_profiles')
     uid = models.CharField(max_length=255, blank=False, null=False)
     oauth_access_token = models.CharField(max_length=255, blank=False, null=False)
     
