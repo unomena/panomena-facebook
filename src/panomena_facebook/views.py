3e04b905e72e58cf6cb215cc6ce84413c90e3ca3
Jurie-Jan Botha <juriejanbotha@gmail.com>
2011-07-05 18:56:11 +0200

    Changed to using class based register view

--- src/panomena_facebook/views.py
+++ src/panomena_facebook/views.py
@@ -8,65 +8,89 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
 from panomena_general.utils import class_from_string, SettingsFetcher, \
-    json_response, is_ajax_request
+    json_response, json_redirect, is_ajax_request
 
 
 settings = SettingsFetcher('facebook')
 
 
-def register(request):
+class RegisterView(object):
     """Handles the registration of a user that is already
     logged in to facebook.
     
     """
-    context = RequestContext(request)
-    # retrieve facebook data from cookie
-    app_id = settings.FACEBOOK_APP_ID
-    secret_key = settings.FACEBOOK_SECRET_KEY
-    fbdata = facebook.get_user_from_cookie(request.COOKIES, app_id, secret_key)
-    if fbdata is None:
-        context['message'] = _('No Facebook data found in cookie. You have ' \
-            'porbably denied the Facebook application access to you account ' \
-            'or have not logged in.')
-        return render_to_response('facebook/failed.html', context)
-    # get some user data
-    try:
-        graph = facebook.GraphAPI(fbdata['access_token'])
-        fb_user_data = graph.get_object('me')
-    except facebook.GraphAPIError, e:
-        context['message'] = e.message
-        return render_to_response('facebook/failed.html', context)
-    # attempt to authenticate the user with uid and access token
-    user = authenticate(
-        uid=fbdata['uid'],
-        oauth_access_token=fbdata['access_token']
-    )
-    # redirect existing user
-    if user:
+
+    def authenticate(self, fbdata):
+        """Authenticate and return the user."""
+        user = authenticate(
+            uid=fbdata['uid'],
+            oauth_access_token=fbdata['access_token'],
+        )
+        return user
+
+    def valid(self, request, form, fbdata):
+        """Process a valid registration form."""
+        user = form.save()
+        user = self.authenticate(fbdata)
         login(request, user)
-        return redirect(settings.LOGIN_REDIRECT_URL)
-    # retrieve the form class to use from settings
-    form_class = settings.FACEBOOK_REGISTER_FORM
-    form_class = class_from_string(form_class)
-    # handle the form post
-    if request.method == 'POST':
-        form = form_class(fbdata, fb_user_data, request.POST, user=user)
-        if form.is_valid():
-            user = form.save()
-            user = authenticate(
-                uid=fbdata['uid'],
-                oauth_access_token=fbdata['access_token'],
-            )
+        # redirect appropriately
+        url = settings.LOGIN_REDIRECT_URL
+        ajax = is_ajax_request(request)
+        if ajax: response = json_redirect(url)
+        else: response = redirect(url)
+        # return the response
+        return response
+
+    def form(self):
+        """Return the form class to use in the view."""
+        return class_from_string(settings.FACEBOOK_REGISTER_FORM)
+
+    def __call__(self, request):
+        """Override to check for facebook data and react accordingly."""
+        context = RequestContext(request)
+        # retrieve facebook data from cookie
+        app_id = settings.FACEBOOK_APP_ID
+        secret_key = settings.FACEBOOK_SECRET_KEY
+        fbdata = facebook.get_user_from_cookie(
+            request.COOKIES, app_id, secret_key
+        )
+        if fbdata is None:
+            context['message'] = _('No Facebook data found in cookie. You ' \
+                'have porbably denied the Facebook application access to you ' \
+                'account or have not logged in.')
+            return render_to_response('facebook/failed.html', context)
+        # get facebook user data
+        try:
+            graph = facebook.GraphAPI(fbdata['access_token'])
+            fb_user_data = graph.get_object('me')
+        except facebook.GraphAPIError, e:
+            context['message'] = e.message
+            return render_to_response('facebook/failed.html', context)
+        # attempt to authenticate the user with uid and access token
+        user = authenticate(
+            uid=fbdata['uid'],
+            oauth_access_token=fbdata['access_token']
+        )
+        # redirect existing user
+        if user:
             login(request, user)
-            url = settings.LOGIN_REDIRECT_URL
-            return redirect(url)
-    else:
-        form = form_class(fbdata, fb_user_data, user=user)
-    context.update({
-        'form': form,
-        'fb_user_data': fb_user_data,
-    })
-    return render_to_response('facebook/register.html', context)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        # handle the form post
+        form_class = self.form()
+        if request.method == 'POST':
+            form = form_class(fbdata, fb_user_data, request.POST, user=user)
+            if form.is_valid():
+                return self.valid(request, form, fbdata)
+        else:
+            form = form_class(fbdata, fb_user_data, user=user)
+        context.update({
+            'form': form,
+            'fb_user_data': fb_user_data,
+        })
+        return render_to_response('facebook/register.html', context)
+
+
+register = RegisterView()
 
 
 @login_required
