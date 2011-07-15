e6be748964c4a322f749616648b155b846a39b1b
Jurie-Jan Botha <juriejanbotha@gmail.com>
2011-07-15 08:38:06 +0200

    Added signed request parser

---
+++ src/panomena_facebook/utils.py
@@ -0,0 +1,22 @@
+import json
+import hmac
+import hashlib
+
+from panomena_general import base64_url_decode
+
+
+def parse_signed_request(signed_request, secret):
+    l = signed_request.split('.', 2)
+    encoded_sig = l[0]
+    payload = l[1]
+    sig = base64_url_decode(encoded_sig)
+    data = json.loads(base64_url_decode(payload))
+    if data.get('algorithm').upper() != 'HMAC-SHA256':
+        return None
+    else:
+        expected_sig = hmac.new(secret, msg=payload, digestmod=hashlib.sha256).digest()
+    if sig != expected_sig:
+        return None
+    else:
+        return data
+
