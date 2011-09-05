6d12a46c83877f603d371dfa5158323d31db0839
Jurie-Jan Botha <juriejanbotha@gmail.com>
2011-09-05 12:56:39 +0200

    Bumped accounts version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='panomena-facebook',
     description='Panomena Facebook',
-    version='0.0.1.1',
+    version='0.0.2',
     author='',
     license='Proprietory',
     url='http://www.unomena.com/',
@@ -11,11 +11,11 @@ setup(
     package_dir = {'': 'src'},
     dependency_links = [
         'http://github.com/unomena/panomena-general/tarball/master#egg=panomena-general',
-        'http://github.com/unomena/panomena-accounts/tarball/master#egg=panomena-accounts',
+        'http://github.com/unomena/panomena-accounts/tarball/0.0.2#egg=panomena_accounts-0.0.2',
     ],
     install_requires = [
         'Django==1.2.5',
         'panomena-general==0.0.1',
-        'panomena-accounts==0.0.1',
+        'panomena-accounts==0.0.2',
     ],
 )
