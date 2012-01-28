3fed982e281cf15e79c35883e2b95dfd8c124dc7
Euan Jonker <euan@unomena.com>
2012-01-28 14:43:18 +0200

    Dropped the dependency links and upped the version to 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='panomena-facebook',
     description='Panomena Facebook',
-    version='0.0.2',
+    version='0.0.3',
     author='',
     license='Proprietory',
     url='http://www.unomena.com/',
@@ -11,8 +11,6 @@ setup(
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     dependency_links = [
-        'http://github.com/unomena/panomena-general/tarball/master#egg=panomena-general',
-        'http://github.com/unomena/panomena-accounts/tarball/0.0.2#egg=panomena_accounts-0.0.2',
     ],
     install_requires = [
         'Django',
